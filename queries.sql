c54f0d98cae24f60c095a7a336fed33b81ec794b
ZeberMVP <rubeen722@gmail.com>
2023-02-03 10:06:55 +0100

    fix/queries.sql

--- queries.sql
+++ queries.sql
@@ -7,10 +7,11 @@ CREATE TABLE users (
 );
 CREATE TABLE favorites (
     user_id INT NOT NULL,
-    movie_id serial 
+    movie_id serial,
     movie_title VARCHAR ( 255 ) NOT NULL,
     date_added TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    movie_poster VARCHAR ( 255 ) NOT NULL,
     PRIMARY KEY (user_id, movie_id),
-    FOREIGN KEY (user_id),
+    FOREIGN KEY (user_id)
         REFERENCES users (user_id)
 );
