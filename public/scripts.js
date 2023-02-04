48539d06b2f2fd332e7434c25369afeb645a555c
ZeberMVP <rubeen722@gmail.com>
2023-02-04 13:52:13 +0100

    add/edit and delete buttons

--- public/scripts.js
+++ public/scripts.js
@@ -30,34 +30,41 @@ if (document.querySelector('.movie-details')) {
 // Manage editMovie put request
 
 if (document.title === 'Edit movie') {
-const editMovieForm = document.querySelector('#editmovie-form');
-const Title = document.querySelector("#editmovie-form > input[type=text]:nth-child(3)").value
-const Year = document.querySelector("#editmovie-form > input[type=text]:nth-child(7)").value
-const Runtime = document.querySelector("#editmovie-form > input[type=text]:nth-child(11)").value
-const Director = document.querySelector("#editmovie-form > input[type=text]:nth-child(15)").value
-const Writer = document.querySelector("#editmovie-form > input[type=text]:nth-child(19)").value
-const Actors = document.querySelector("#editmovie-form > input[type=text]:nth-child(23)").value
-const Plot = document.querySelector("#editmovie-form > input[type=text]:nth-child(27)").value
-const Poster = document.querySelector("#editmovie-form > input[type=text]:nth-child(31)").value
+    const Title = document.querySelector("#editmovie-form > input[type=text]:nth-child(3)").value
+    const Year = document.querySelector("#editmovie-form > input[type=text]:nth-child(7)").value
+    const Runtime = document.querySelector("#editmovie-form > input[type=text]:nth-child(11)").value
+    const Director = document.querySelector("#editmovie-form > input[type=text]:nth-child(15)").value
+    const Writer = document.querySelector("#editmovie-form > input[type=text]:nth-child(19)").value
+    const Actors = document.querySelector("#editmovie-form > input[type=text]:nth-child(23)").value
+    const Plot = document.querySelector("#editmovie-form > input[type=text]:nth-child(27)").value
+    const Poster = document.querySelector("#editmovie-form > input[type=text]:nth-child(31)").value
 
-form.addEventListener('submit', (event) => {
-    event.preventDefault();
+    form.addEventListener('submit', (event) => {
+        event.preventDefault();
 
-    fetch(`https://localhost:3000/admin/editmovie/${title}`, {
-        method: 'PUT',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            Title,
-            Year ,
-            Runtime ,
-            Director ,
-            Writer ,
-            Actors ,
-            Plot ,
-            Poster 
+        fetch(`https://localhost:3000/admin/editmovie/${title}`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify({
+                Title,
+                Year,
+                Runtime,
+                Director,
+                Writer,
+                Actors,
+                Plot,
+                Poster
+            })
         })
+    });
+}
+
+// Delete function for buttton remove in admin page
+const deleteMovieButton = (deleteRoute) => {
+    fetch(deleteRoute, {
+        method: 'DELETE'
     })
-});
+        .then(location.reload())
 }
