7242a3b2cebf06eacc6c918464237e8757c0e18d
Francsy <francsyludens@gmail.com>
2023-02-06 13:25:00 +0100

    add / function to middleware that create the cookie just if the cookie is invalid

--- middlewares/verifiedToken.js
+++ middlewares/verifiedToken.js
@@ -11,6 +11,8 @@ adminProtector.use((req, res, next) => {
     const token = req.cookies['access-token'];
     if (token) {
         jwt.verify(token, jwt_key, async (err, decoded) => {
+            // console.log(err)
+            // Si está caducado lo elimina
             let data = await users.getRole(decoded.email);
             const { role, logged_in } = data[0];
             if (logged_in == true && role === "admin") {
@@ -47,24 +49,30 @@ userProtector.use((req, res, next) => {
     }
 });
 
-
 userProtectorAndRefresh.use((req, res, next) => {
     const token = req.cookies['access-token'];
     if (token) {
         jwt.verify(token, jwt_key, async (err, decoded) => {
             let data = await users.getRole(decoded.email);
             const { role, logged_in } = data[0];
-            if (logged_in == true && role === "user") {
+            if (err) { // Si ha caducado vamos a comprobar si está logged_in en la DB para darle un token nuevo y dejar continuar si es user
+                if (logged_in == true && role === "user") {
+                    const userForToken = {
+                        email: decoded.email,
+                        id: decoded.id
+                    };
+                    const newToken = jwt.sign(userForToken, jwt_key, { expiresIn: '20m' });
+                    res.cookie('access-token', newToken, {
+                        httpOnly: true
+                        // secure: process.env.NODE_ENV === "production"
+                    });
+                    req.decoded = decoded;
+                    next();
+                } else {
+                    return res.json({ msg: 'Invalid token' });
+                }
+            } else if (logged_in == true && role === "user") {
                 req.decoded = decoded;
-                const userForToken = {
-                    email: decoded.email,
-                    id: decoded.id
-                };
-                const newToken = jwt.sign(userForToken, jwt_key, { expiresIn: '20m' });
-                res.cookie('access-token', newToken, {
-                    httpOnly: true
-                    // secure: process.env.NODE_ENV === "production"
-                })
                 next();
             } else {
                 return res.json({ msg: 'Invalid token' });
@@ -76,6 +84,8 @@ userProtectorAndRefresh.use((req, res, next) => {
         });
     }
 });
+
+
 
 module.exports = {
     adminProtector,
