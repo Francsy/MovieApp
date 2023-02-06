7883a740e8609de0d8b6561322380f10370a178c
Francsy <francsyludens@gmail.com>
2023-02-06 23:17:58 +0100

    refactor/ logout verifiedToken function

--- controllers/authController.js
+++ controllers/authController.js
@@ -6,6 +6,7 @@ const jwt = require('jsonwebtoken');
 const jwt_key = process.env.JWT_KEY;
 
 
+
 // Renderiza pagina inicial con formulario de autenticación: 
 
 const renderLogin = (req, res) => {
@@ -69,8 +70,8 @@ const postSignUp = async (req, res) => {
 
 const logOut = async (req, res) => {
     try {
-        console.log(req)
-        await users.changeStatusToFalse(req)
+        console.log(req.decoded.email)
+        await users.changeStatusToFalse(req.decoded.email)
         res.clearCookie('access-token').redirect('/');
     } catch (err) {
         console.log(err)
@@ -91,7 +92,6 @@ const postRecoverPassword = (req, res) => {
 
 // const googleLogin?
 // const googleSingUp
-// const logOut
 
 
 module.exports = {
--- middlewares/verifiedToken.js
+++ middlewares/verifiedToken.js
@@ -5,68 +5,44 @@ const jwt_key = process.env.JWT_KEY;
 const authController = require('../controllers/authController')
 
 const adminProtector = express.Router();
-const userProtector = express.Router(); // Esto se puede unificar
-const getEmailForLogOutAndRefresh = express.Router();
-const userProtectorAndRefresh = express.Router()
+const userProtector = express.Router();
+const logOutProtector = express.Router();
 
-adminProtector.use((req, res, next) => {
-    const token = req.cookies['access-token'];
-    if (token) {
-        jwt.verify(token, jwt_key, async (err, decoded) => {
-            // console.log(err)
-            // Si está caducado lo elimina
-            let data = await users.getRole(decoded.email);
-            const { role, logged_in } = data[0];
-            if (logged_in == true && role === "admin") {
-                req.decoded = decoded;
-                next();
-            } else {
-                return res.json({ msg: 'Invalid token' });
-            }
-        });
-    } else {
-        res.send({
-            msg: 'Token not provided'
-        });
-    }
-});
-
-userProtector.use((req, res, next) => {
-    const token = req.cookies['access-token'];
-    if (token) {
-        jwt.verify(token, jwt_key, async (err, decoded) => {
-            let data = await users.getRole(decoded.email);
-            const { role, logged_in } = data[0];
-            if (logged_in == true && role === "user") {
-                req.decoded = decoded;
-                next();
-            } else {
-                return res.json({ msg: 'Invalid token' });
-            }
-        });
-    } else {
-        res.send({
-            msg: 'Token not provided'
-        });
-    }
-});
-
-getEmailForLogOutAndRefresh.use((req, res, next) => {
-    const token = req.cookies['access-token'];
-    jwt.verify(token, jwt_key, async (err, decoded) => {
-        authController.logOut(decoded.email, res)
-    });
-});
-
-
-userProtectorAndRefresh.use(async (req, res, next) => {
+adminProtector.use(async (req, res, next) => {
     const token = req.cookies['access-token'];
     if (!token) return res.json({ msg: 'Token not provided' });
     try {
         const decoded = jwt.verify(token, jwt_key);
     
         if ((decoded.exp * 1000) < Date.now()) return res.json({ msg: 'Expired token' });
+        
+        const userData = await users.getRole(decoded.email);
+        if (!userData || userData[0].logged_in !== true || userData[0].role !== "admin") return res.json({ msg: 'Invalid token' });
 
+        const userForToken = {
+            email: decoded.email,
+            id: decoded.id
+        };
+        const newToken = jwt.sign(userForToken, jwt_key, { expiresIn: '20m' });
+        res.cookie('access-token', newToken, {
+            httpOnly: true,
+            // secure: process.env.NODE_ENV === "production"
+        });
+        req.decoded = decoded;
+        next();
+    } catch (error) {
+        return res.json({ msg: 'Invalid token' });
+    }
+});
+
+userProtector.use(async (req, res, next) => {
+    const token = req.cookies['access-token'];
+    if (!token) return res.json({ msg: 'Token not provided' });
+    try {
+        const decoded = jwt.verify(token, jwt_key);
+    
+        if ((decoded.exp * 1000) < Date.now()) return res.json({ msg: 'Expired token' });
+        
         const userData = await users.getRole(decoded.email);
         if (!userData || userData[0].logged_in !== true || userData[0].role !== "user") return res.json({ msg: 'Invalid token' });
 
@@ -86,11 +62,23 @@ userProtectorAndRefresh.use(async (req, res, next) => {
     }
 });
 
-
+logOutProtector.use(async (req, res, next) => {
+    const token = req.cookies['access-token'];
+    if (!token) return res.json({ msg: 'Token not provided' });
+    try {
+        const decoded = jwt.verify(token, jwt_key);
+        if ((decoded.exp * 1000) < Date.now()) return res.json({ msg: 'Expired token' });
+        const userData = await users.getRole(decoded.email);
+        if (!userData || userData[0].logged_in !== true) return res.json({ msg: 'Invalid token' });
+        req.decoded = decoded;
+        next();
+    } catch (error) {
+        return res.json({ msg: 'Invalid token' });
+    }
+});
 
 module.exports = {
     adminProtector,
     userProtector,
-    userProtectorAndRefresh,
-    getEmailForLogOutAndRefresh
+    logOutProtector
 }
